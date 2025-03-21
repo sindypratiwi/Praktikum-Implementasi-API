7bf3a54833cb0bd24a394434388ba5d54df8aaf1
sindypratiwi <luhgedesindypratiwi12@gmail.com>
2025-03-21 11:42:18 +0800

    mengganti localhost menjadi 4000

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res)=>{
   res.writeHead(200, {'Comtent-Type': 'text/plain'});
   res.end('Hello, World');
 });
-server.listen(3000, () => console.log('server berjalan di http://localhost:3000'));
+server.listen(4000, () => console.log('server berjalan di http://localhost:4000'));
